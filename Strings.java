27ef564441de81a5162584528d6cede1003b7d55
Zaid Fakecrafter <fakecrafter@protonmail.com>
2023-09-23 22:07:22 +0200

    worked on recursive string exercises

---
+++ Strings.java
@@ -0,0 +1,38 @@
+public class Algs {
+	public String umkehren(String s) {
+		if(s.length() <= 1) {
+			return s;
+		}
+		return umkehren(s.substring(1)) + s[0];
+	}
+	public String glaetten(String pText, int n) {
+		if(n == pText.length()) {return pText}
+		if(Character.isLetter(pText.charAt(n))) {return glaetten(pText, n+1);}
+		return glaetten(pText.substring(0,n) + pText.substring(n+1), n+1)
+	}
+	public boolean palindrom(String s) {
+		if(s.compareTo(umkehren(s)) == 0) {return true;}
+		return false;
+	}
+	public String verwandelInGrossbuchstaben(String s, int n) {
+		if(n == s.length) {return s;}
+		s[n] = (char) ((int) s.charAt(n)) - 32;
+		return verwandelInGrossbuchstaben(s, n+1);
+	}
+    public String stripSpaces(String s) {
+        if(s.equals(" ")) {return "";}
+        if(s.length() == 1) {return s;}
+        if(s.charAt(0) == ' ') {return stripSpaces(s.substring(1););}
+        return s.substring(0,1) + stripSpaces(s.substring(1))
+    }
+    public int lsnr(String s, int compare, int i) {
+        System.out.println("just got called with lsnr(" + s + ", " + compare + ", " + i + ");");
+        if(s.length() <= 1) {return s.length();}
+        if(compare == s.length()) {return s.length();}
+        if(i == s.length()) {return lsnr(s, ++compare, ++compare);}
+        if(s.charAt(compare) == s.charAt(i)) {
+            return Math.max(lsnr(s.substring(0,i), 0, 1), lsnr(s.substring(++compare), 0, 1));
+        }
+        return lsnr(s, compare, ++i);
+    }
+}
