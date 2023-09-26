2b4283a2a774094f9ac36c1662f0bdc9400f7c0b
Fakecrafter <67643686+Fakecrafter@users.noreply.github.com>
2023-09-26 08:42:38 +0200

    fix compare statement in Strings.java

--- Strings.java
+++ Strings.java
@@ -29,12 +29,12 @@ public class Algs {
         System.out.println("just got called with lsnr(" + s + ", " + compare + ", " + i + ");");
         if(s.length() <= 1) {return s.length();}
         if(compare == s.length()) {return s.length();}
-        if(i == s.length()) {return lsnr(s, ++compare, ++compare);}
+        if(i == s.length()) {return lsnr(s, compare+1, compare+2);}
         if(s.charAt(compare) == s.charAt(i)) {
             // potential optimization because if the first one return something longer
             // than the second parts length why calculate the second part at all?
-            return Math.max(lsnr(s.substring(0,i), 0, 1), lsnr(s.substring(++compare), 0, 1));
+            return Math.max(lsnr(s.substring(0,i), 0, 1), lsnr(s.substring(compare+1), 0, 1));
         }
-        return lsnr(s, compare, ++i);
+        return lsnr(s, compare, i+1);
     }
 }
